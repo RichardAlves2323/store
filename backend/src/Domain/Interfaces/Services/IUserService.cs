a8723b39ef53838bab2a50bd9898a69b4fc9cc05
Richard Alves <richardalves232323@gmail.com>
2025-09-27 16:23:24 -0300

    feat: Add user service interface and implementation

---
+++ backend/src/Domain/Interfaces/Services/IUserService.cs
@@ -0,0 +1,10 @@
+using Domain.Entities;
+
+namespace Domain.Interfaces.Services;
+
+public interface IUserService
+{
+    Task<User> GetByIdAsync(int id);
+    Task<User> GetByEmailAsync(string email);
+    Task<User> AddAsync(User user);
+}

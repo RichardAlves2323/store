8a70c42df8a55609969ee3d0ba1fbd98a5ab34e0
Richard Alves <richardalves232323@gmail.com>
2025-09-26 22:12:02 -0300

    feat:  Add product repository interface

--- backend/src/Domain/Entities/Product.cs
+++ backend/src/Domain/Entities/Product.cs
@@ -3,7 +3,7 @@ public class Product
 {
     public int Id { get; private set; }
     public string Name { get; private set; }
-    public string Description { get; private set; } = string.Empty;
+    public string Description { get; private set; }
     public decimal Price { get; private set; }
 
     public Product(string name, string description, decimal price)

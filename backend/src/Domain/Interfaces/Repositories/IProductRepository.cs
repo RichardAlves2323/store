5a94fbe0e006a48884076eb0234b3b414a123fcc
Richard Alves <richardalves232323@gmail.com>
2025-09-27 00:07:51 -0300

    feat: Add product repository interface

--- backend/src/Domain/Interfaces/Repositories/IProductRepository.cs
+++ backend/src/Domain/Interfaces/Repositories/IProductRepository.cs
@@ -2,9 +2,9 @@ using Domain.Entities;
 namespace Domain.Interfaces.Repositories;
 public interface IProductRepository
 {
-    Task<Product> GetByIdAsync(int id);
+    Task<Product?> GetByIdAsync(int id);
     Task<IEnumerable<Product>> GetAllAsync();
-    Task AddAsync(Product product);
-    Task UpdateAsync(Product product);
+    Task<Product> AddAsync(Product product);
+    Task<Product> UpdateAsync(Product product);
     Task DeleteAsync(int id);
 }             

e6c85ffa99a9173a021edb1e8868a24cb751dda4
Richard Alves <richardalves232323@gmail.com>
2025-09-27 16:05:49 -0300

    feat:  Add product controller

---
+++ backend/src/Api/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+using Domain.Entities;
+using Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var product = await _productService.GetByIdAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return Ok(product);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var products = await _productService.GetAllAsync();
+        return Ok(products);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Product product)
+    {
+        var createdProduct = await _productService.AddAsync(product);
+        return CreatedAtAction(nameof(GetById), new { id = createdProduct.Id }, createdProduct);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, Product product)
+    {
+        if (id != product.Id)
+        {
+            return BadRequest();
+        }
+
+        var updatedProduct = await _productService.UpdateAsync(product);
+        return Ok(updatedProduct);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _productService.DeleteAsync(id);
+        return NoContent();
+    }
+}
